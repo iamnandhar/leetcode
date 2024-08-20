5d49ceace537b5ca068e924f0afa3465efb35584
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-20 22:31:52 +0530

    Leet code 1480 - simplified further

--- Basics/RunningSum1480.java
+++ Basics/RunningSum1480.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 class RunningSum1480 {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
+        int[] nums = {1, 2, 3, 4};
         for (int i = 1; i < nums.length; i++) {
-            nums[i] = nums[i] + nums[i-1];
+            nums[i] += nums[i - 1];
         }
         System.out.println(Arrays.toString(nums));
     }
