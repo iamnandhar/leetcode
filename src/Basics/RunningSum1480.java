a02f32e84f51390be12e574d7a373a870012fb19
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-21 22:07:47 +0530

    Tests for 1480

--- src/Basics/RunningSum1480.java
+++ src/Basics/RunningSum1480.java
@@ -2,12 +2,17 @@ package src.Basics;
 
 import java.util.Arrays;
 
-class RunningSum1480 {
+public class RunningSum1480 {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4};
+        calculateRunningSum(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int[] calculateRunningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             nums[i] += nums[i - 1];
         }
-        System.out.println(Arrays.toString(nums));
+        return nums;
     }
 }
---
+++ test/Basics/RunningSum1480Test.java
@@ -0,0 +1,44 @@
+package test.Basics;
+
+import org.junit.jupiter.api.Test;
+import src.Basics.RunningSum1480;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class RunningSum1480Test {
+
+    @Test
+    void calculatesRunningSumForMultipleElements() {
+        int[] nums = {1, 2, 3, 4};
+        int[] expected = {1, 3, 6, 10};
+        assertArrayEquals(expected, RunningSum1480.calculateRunningSum(nums));
+    }
+
+    @Test
+    void calculatesRunningSumForSingleElement() {
+        int[] nums = {5};
+        int[] expected = {5};
+        assertArrayEquals(expected, RunningSum1480.calculateRunningSum(nums));
+    }
+
+    @Test
+    void calculatesRunningSumForEmptyArray() {
+        int[] nums = {};
+        int[] expected = {};
+        assertArrayEquals(expected, RunningSum1480.calculateRunningSum(nums));
+    }
+
+    @Test
+    void calculatesRunningSumForArrayWithZeroes() {
+        int[] nums = {0, 0, 0, 0};
+        int[] expected = {0, 0, 0, 0};
+        assertArrayEquals(expected, RunningSum1480.calculateRunningSum(nums));
+    }
+
+    @Test
+    void calculatesRunningSumForArrayWithNegativeNumbers() {
+        int[] nums = {-1, -2, -3, -4};
+        int[] expected = {-1, -3, -6, -10};
+        assertArrayEquals(expected, RunningSum1480.calculateRunningSum(nums));
+    }
+}
