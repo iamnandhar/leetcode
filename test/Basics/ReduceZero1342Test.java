26c5ee9ce3cbf868ecf394352c92ec5edd35688b
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-22 21:58:12 +0530

    Reduce Zero 1342 soln with junit

---
+++ test/Basics/ReduceZero1342Test.java
@@ -0,0 +1,33 @@
+package test.Basics;
+
+import org.junit.jupiter.api.Test;
+import src.Basics.ReduceZero1342;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReduceZero1342Test {
+    @Test
+    void returnsZeroStepsForZero() {
+        assertEquals(0, ReduceZero1342.numberOfSteps(0));
+    }
+
+    @Test
+    void returnsOneStepForOne() {
+        assertEquals(1, ReduceZero1342.numberOfSteps(1));
+    }
+
+    @Test
+    void returnsCorrectStepsForEvenNumber() {
+        assertEquals(6, ReduceZero1342.numberOfSteps(14));
+    }
+
+    @Test
+    void returnsCorrectStepsForOddNumber() {
+        assertEquals(5, ReduceZero1342.numberOfSteps(7));
+    }
+
+    @Test
+    void returnsCorrectStepsForLargeNumber() {
+        assertEquals(12, ReduceZero1342.numberOfSteps(123));
+    }
+}
