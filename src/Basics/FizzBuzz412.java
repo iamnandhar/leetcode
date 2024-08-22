7e6147f6843194ad26d0242f88adfb63f79cae45
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-22 21:29:29 +0530

    Solution for fizz buzz 412

--- src/Basics/FizzBuzz412.java
+++ src/Basics/FizzBuzz412.java
@@ -8,20 +8,20 @@ public class FizzBuzz412 {
         System.out.println(fizzBuzz(15));
     }
     public static List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            String str = "";
-            if (i % 3 == 0) {
-                str += "Fizz";
-            }
-            if (i % 5 == 0) {
-                str += "Buzz";
-            }
-            if (str.isEmpty()) {
-                str += i;
-            }
-            result.add(str);
+    List<String> result = new ArrayList<>();
+    for (int i = 1; i <= n; i++) {
+        StringBuilder str = new StringBuilder();
+        if (i % 3 == 0) {
+            str.append("Fizz");
         }
-        return result;
+        if (i % 5 == 0) {
+            str.append("Buzz");
+        }
+        if (str.isEmpty()) {
+            str.append(i);
+        }
+        result.add(str.toString());
     }
+    return result;
+}
 }
