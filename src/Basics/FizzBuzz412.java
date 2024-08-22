01689c67e3cde1d0e01890c44db6b04cded21f25
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-22 21:35:38 +0530

    Junit for fizz buzz 412

--- src/Basics/FizzBuzz412.java
+++ src/Basics/FizzBuzz412.java
@@ -7,21 +7,22 @@ public class FizzBuzz412 {
     public static void main(String[] args) {
         System.out.println(fizzBuzz(15));
     }
+
     public static List<String> fizzBuzz(int n) {
-    List<String> result = new ArrayList<>();
-    for (int i = 1; i <= n; i++) {
-        StringBuilder str = new StringBuilder();
-        if (i % 3 == 0) {
-            str.append("Fizz");
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            StringBuilder str = new StringBuilder();
+            if (i % 3 == 0) {
+                str.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                str.append("Buzz");
+            }
+            if (str.isEmpty()) {
+                str.append(i);
+            }
+            result.add(str.toString());
         }
-        if (i % 5 == 0) {
-            str.append("Buzz");
-        }
-        if (str.isEmpty()) {
-            str.append(i);
-        }
-        result.add(str.toString());
+        return result;
     }
-    return result;
-}
 }
---
+++ test/Basics/FizzBuzz412Test.java
@@ -0,0 +1,47 @@
+package test.Basics;
+
+import org.junit.jupiter.api.Test;
+import src.Basics.FizzBuzz412;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzz412Test {
+    @Test
+    void generatesFizzBuzzForRange() {
+        List<String> expected = Arrays.asList("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz");
+        assertEquals(expected, FizzBuzz412.fizzBuzz(15));
+    }
+
+    @Test
+    void generatesFizzBuzzForSingleNumber() {
+        List<String> expected = List.of("1");
+        assertEquals(expected, FizzBuzz412.fizzBuzz(1));
+    }
+
+    @Test
+    void generatesFizzBuzzForNumberDivisibleByThree() {
+        List<String> expected = Arrays.asList("1", "2", "Fizz");
+        assertEquals(expected, FizzBuzz412.fizzBuzz(3));
+    }
+
+    @Test
+    void generatesFizzBuzzForNumberDivisibleByFive() {
+        List<String> expected = Arrays.asList("1", "2", "Fizz", "4", "Buzz");
+        assertEquals(expected, FizzBuzz412.fizzBuzz(5));
+    }
+
+    @Test
+    void generatesFizzBuzzForNumberDivisibleByThreeAndFive() {
+        List<String> expected = Arrays.asList("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz");
+        assertEquals(expected, FizzBuzz412.fizzBuzz(15));
+    }
+
+    @Test
+    void generatesFizzBuzzForZero() {
+        List<String> expected = List.of();
+        assertEquals(expected, FizzBuzz412.fizzBuzz(0));
+    }
+}
