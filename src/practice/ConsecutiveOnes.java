1aded9bf9ba51993fbf61074a09681a1f2b23022
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-25 23:02:21 +0530

    Consecutive ones efficient time

--- src/practice/ConsecutiveOnes.java
+++ src/practice/ConsecutiveOnes.java
@@ -11,11 +11,11 @@ public class ConsecutiveOnes {
         for (int num : nums) {
             if (num == 1) {
                 count++;
-                max = Math.max(max, count);
             } else {
+                max = Math.max(max, count);
                 count = 0;
             }
         }
-        return max;
+        return Math.max(max, count);
     }
 }
