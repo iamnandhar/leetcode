e269c9f569be9abb172928ac32de4d5abd0c08a0
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-25 22:56:48 +0530

    Consecutive ones

---
+++ src/practice/ConsecutiveOnes.java
@@ -0,0 +1,21 @@
+package src.practice;
+
+public class ConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
