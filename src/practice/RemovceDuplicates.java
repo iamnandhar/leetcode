7f3214b3c314b98a41fa326b13a240e83b5040d7
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-27 22:02:26 +0530

    Remove duplicates in array

---
+++ src/practice/RemovceDuplicates.java
@@ -0,0 +1,19 @@
+package src.practice;
+
+public class RemovceDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int len = removeDuplicates(nums);
+        System.out.println(len);
+    }
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j=1; j<nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
