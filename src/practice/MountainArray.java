55f80c1211489235a1e461983b5c8eaf88374ff4
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-28 20:54:23 +0530

    Valid mountain array

---
+++ src/practice/MountainArray.java
@@ -0,0 +1,22 @@
+package src.practice;
+
+public class MountainArray {
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 0};
+        System.out.println(validMountainArray(arr));
+    }
+    public static boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+        if (i == 0 || i == n - 1) {
+            return false;
+        }
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+        return i == n - 1;
+    }
+}
