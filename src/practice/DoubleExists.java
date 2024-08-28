4d71f30f6629958649c0eb4aa07c3348e9867a98
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-28 20:47:59 +0530

    Double exists refactored for efficiency

--- src/practice/DoubleExists.java
+++ src/practice/DoubleExists.java
@@ -1,5 +1,7 @@
 package src.practice;
 
+import java.util.HashSet;
+
 public class DoubleExists {
     public static void main(String[] args) {
         int[] arr = {10, 2, 5, 3};
@@ -7,12 +9,12 @@ public class DoubleExists {
     }
 
     public static boolean checkIfExist(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
-                    return true;
-                }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(2 * num) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
             }
+            set.add(num);
         }
         return false;
     }
