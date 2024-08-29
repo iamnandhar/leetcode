d92da543818511e4dce8aaa8e5c53c661905862f
nandhakumar Ramanujan <nandha88@gmail.com>
2024-08-29 22:06:20 +0530

    Greates Element array problem

---
+++ src/arrays/GreatestElement.java
@@ -0,0 +1,23 @@
+package src.arrays;
+
+import java.util.Arrays;
+
+public class GreatestElement {
+    public static void main(String[] args) {
+        int[] arr = { 17,18,5,4,6,1};
+        System.out.println(Arrays.toString(greatestElement(arr)));
+    }
+    public static int[] greatestElement(int[] arr) {
+        int n = arr.length;
+        int max = arr[n-1];
+        arr[n-1] = -1;
+        for (int i = n-2; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return arr;
+    }
+}
