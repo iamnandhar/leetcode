43a49d1edaf5bfcece7bbb6e039d01926677200f
nandhakumar Ramanujan <nandha88@gmail.com>
2024-09-13 22:43:20 +0530

    Two sums leet code soln

--- src/problems/TwoSums.java
+++ src/problems/TwoSums.java
@@ -16,7 +16,7 @@ public class TwoSums {
     }
 
     public static void main(String[] args) {
-        int nums[] = {2, 7, 11, 15};
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
         TwoSums twoSums = new TwoSums();
         int[] result = twoSums.twoSum(nums, target);
